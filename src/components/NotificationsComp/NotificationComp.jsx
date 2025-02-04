cd617535d1b93701f3f62275fe23b936a2b92cae
Devanshu Roy <devanshu20000@gmail.com>
2025-02-05 02:38:44 +0530

    Updated NotificationComp, fixed critical issue.

--- src/components/NotificationsComp/NotificationComp.jsx
+++ src/components/NotificationsComp/NotificationComp.jsx
@@ -1,8 +1,10 @@
 const NotificationComp = (props) => {
+  console.log(props.notifData);
   return (
     <>
       <header className="mx-[30%] font-semibold text-2xl">
-        You have some notifications!
+        You have {props.notifData.length == 0 ? `no` : `some`} notifications
+        {props.notifData.length == 0 ? `.` : `!`}
       </header>
       <div className="bg-white mx-[30%] p-5 h-[45vh]">
         <ul className="list-none">
